ae1e0006c2d49f51ffd7f18379ce20522b02ceb8
aLxVa <dialexanderx@gmail.com>
2026-06-16 22:56:38 +0200

    fix: explicit manualChunks for react to prevent circular chunk dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -28,5 +28,14 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')) {
+            return 'react-vendor';
+          }
+        },
+      },
+    },
   },
 });
