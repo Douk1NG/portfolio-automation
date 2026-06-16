e49a18e48432474c03d03b3a20f66821117841cb
aLxVa <dialexanderx@gmail.com>
2026-06-16 22:52:19 +0200

    feat: add ErrorBoundary to catch and display UI errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 import { initializeAutosave } from './hooks/useAutosave';
 import './globals.css';
 
@@ -8,6 +9,8 @@ initializeAutosave();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
